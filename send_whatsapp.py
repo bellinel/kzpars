fe5e7471f65246ca129e3c40610a4eae82b9017b
bellinel <artursimoncik@gmail.com>
2025-06-28 13:09:56 +0300

    update: обновлен send_whatsapp.py

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -1,24 +1,39 @@
 import os
 import requests
 import asyncio
+import zipfile
 
 IDISTANSE = "7105271174" 
 API_KEY = "846ea21f0e6a4bd097b938f9956bdefb134268aad3344daab4"
 CHAT_ID = "120363419948813756@g.us"
 
 
+def create_zip_from_folder(folder_path, zip_name):
+    zip_path = f"{zip_name}.zip"
+
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+    print(f"📦 Архив создан: {zip_path}")
+    return zip_path
+
+
 def save_files_whatsapp(file_name):
     url = f"https://7105.api.greenapi.com/waInstance{IDISTANSE}/uploadFile/{API_KEY}"
 
     files = [ 
-        ('file', (file_name, open(f'pdf/{file_name}','rb'),'application/pdf')) 
+        ('file', (file_name, open(file_name,'rb'),'application/x-compressed')) 
     ]
     headers = {
-        'Content-Type': 'application/pdf'
+        'Content-Type': 'application/x-compressed'
     }
     response = requests.post(url, files=files, headers=headers)
-    url = response.text.encode('utf8')
-    url = url.get('urlFile')
+    json_data = response.json()
+    url = json_data.get('urlFile')
     return url
 
 
@@ -38,9 +53,9 @@ def send_file_whatsapp(file_name):
 
 
 async def send_all_files_whatsapp():
-    for file in os.listdir('pdfs'):
-        send_file_whatsapp(file)
-        await asyncio.sleep(1)
+    zip_path = create_zip_from_folder('pdfs', 'pdfs.zip')
+    send_file_whatsapp(zip_path)
+    
 
 
 
